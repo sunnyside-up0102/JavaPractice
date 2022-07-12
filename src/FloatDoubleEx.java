370894f9182bd8d4c669236c29382af6a102131a
sunnyside-up0102 <87693330+sunnyside-up0102@users.noreply.github.com>
2022-07-12 11:27:06 +0900

    FloatDoubleEx

---
+++ src/FloatDoubleEx.java
@@ -0,0 +1,14 @@
+
+public class FloatDoubleEx {
+	public static void main(String[] argc) {
+		
+		float f=3.141592653589793f;
+		double d= 3.141592653589793d;
+		
+		System.out.println("float: " +f);
+		System.out.println("double: " +d);
+		
+		double d2 = 3.141592653589793;//d생략가능
+		System.out.println("double: " +d2);
+	}
+}
