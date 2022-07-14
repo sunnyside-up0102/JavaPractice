0c61d639013569e75626abd0acca25db99d0ec04
sunnyside-up0102 <87693330+sunnyside-up0102@users.noreply.github.com>
2022-07-15 00:05:03 +0900

    0714review

--- src/가위바위보.java
+++ src/가위바위보.java
@@ -4,14 +4,13 @@ public class 가위바위보 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("가위는 1,바위는 2, 보는 3 :");
+		System.out.print("가위는 1,바위는 2, 보는 3 :");
 		int user = scan.nextInt();
 		int com;
-		  for ( com=1; com<4; com++) {
-		      System.out.println(Math.random());
-		   }
-		 int result = com - user;
-		  
+		com = (int) (Math.random() * 2) + 1;
+		System.out.println("컴퓨터는"+(int) (Math.random() * 2) + 1);
+		
+		int result = com - user;
 		switch (result) {
 		case -1 :
 			System.out.println("사용자 win");
