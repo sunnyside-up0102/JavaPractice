b5a1eafba346a65852f8e0ca05e4191e9876a4f5
sunnyside-up0102 <87693330+sunnyside-up0102@users.noreply.github.com>
2022-07-18 00:10:34 +0900

    0717prac

---
+++ src/ArrEx6.java
@@ -0,0 +1,21 @@
+
+public class ArrEx6 {
+
+	public static void main(String[] args) {
+		String [] arrStr = {"DD","SS","AA"};
+		System.out.println(arrStr.length);
+		
+		for(int i=0;i<arrStr.length;i++)
+		{
+			System.out.println(arrStr[i]);
+		}
+		
+		for(String s : arrStr)
+		{
+			System.out.println(s);
+		}
+		
+		
+	}
+
+}
