b23bec945145f8bf986a3125ecce40d749a74116
sunnyside-up0102 <87693330+sunnyside-up0102@users.noreply.github.com>
2022-07-26 23:41:29 +0900

    0726prac

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+	String name;
+	int grade;
+	String department;
+	
+	Student(String name, int grade, String department){
+		this.name = name;
+		this.grade = grade;
+		this.department =department;
+	}
+}
---
+++ src/StudentMain.java
@@ -0,0 +1,13 @@
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student stu = new Student("°ûµµÇõ", 2, "¼ÒÇÐ");
+				
+		System.out.println(stu.name);
+		System.out.println(stu.grade);
+		System.out.println(stu.department);
+		
+	}
+
+}
